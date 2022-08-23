0fab967cb626c47cfc59c74d264705d20ebc1c8d
mahmoodi.exp <mahmoodi.exp@gmail.com>
2022-08-23 06:59:07 +0000

    Soccer field

--- src/components/SoccerField.tsx
+++ src/components/SoccerField.tsx
@@ -1,47 +1,79 @@
 import React from "react";
-import defaultShirt from '../images/default_shirt.png'
-import SoccerFieldImg from "../images/SoccerField.png"
+import SoccerFieldImg from "./../images/SoccerField.png"
+import defaultShirt from "./../images/default_shirt.png"
+import selectedShirt from "./../images/selected_shirt.png"
+import addCircle from "./../images/add-circle.png"
 
-function SoccerField(){
-    return(
+export default function SoccerField() {
+    return (
         <div
-        className="flex flex-col py-10 px-32 w-3/5 min-w-fit bg-cover bg-center bg-no-repeat"
-        style={{ backgroundImage: `url(${SoccerFieldImg})` }}
-
+            className="flex flex-col w-1/2 relative max-w-3xl"
         >
+            <img
+                src={SoccerFieldImg}
+                alt="Soccer field image"
+                className="absolute w-full h-full -z-50"
+            />
             <div
-            className="flex flex-row justify-around"
+                className="flex flex-row justify-around mx-8"
             >
-                <img className="w-1/4" src={defaultShirt} alt="default T-shirt"/>
-                <img className="w-1/4" src={defaultShirt} alt="default T-shirt"/>
+
+
+                <DefaultPlayer />
+                <SelectedPlayer />
             </div>
             <div
-            className="flex flex-row justify-around"
+                className="flex flex-row justify-around mx-6"
             >
-                <img src={defaultShirt} alt="default T-shirt"/>
-                <img src={defaultShirt} alt="default T-shirt"/>
-                <img src={defaultShirt} alt="default T-shirt"/>
-                <img src={defaultShirt} alt="default T-shirt"/>
-                <img src={defaultShirt} alt="default T-shirt"/>
+                <SelectedPlayer />
+                <SelectedPlayer />
+                <SelectedPlayer />
+                <SelectedPlayer />
+                <SelectedPlayer />
+
             </div>
             <div
-            className="flex flex-row justify-around"
+                className="flex flex-row justify-around mx-6"
             >
-                <img src={defaultShirt} alt="default T-shirt"/>
-                <img src={defaultShirt} alt="default T-shirt"/>
-                <img src={defaultShirt} alt="default T-shirt"/>
-                <img src={defaultShirt} alt="default T-shirt"/>
-                <img src={defaultShirt} alt="default T-shirt"/>
+                <SelectedPlayer />
+                <SelectedPlayer />
+                <SelectedPlayer />
+                <SelectedPlayer />
+                <SelectedPlayer />
+
             </div>
             <div
-            className="flex flex-row justify-around"
+                className="flex flex-row justify-around mx-6"
             >
-                <img src={defaultShirt} alt="default T-shirt"/>
-                <img src={defaultShirt} alt="default T-shirt"/>
-                <img src={defaultShirt} alt="default T-shirt"/>
+                <SelectedPlayer />
+                <SelectedPlayer />
+                <SelectedPlayer />
+
             </div>
         </div>
     )
-}
 
-export default SoccerField;
+    function DefaultPlayer() {
+        return (
+            <div
+                className="flex mb-2 items-center justify-center w-1/6 mt-6 relative"
+            >
+                <img className="w-full" src={defaultShirt} alt="default T-shirt" />
+                <img className="h-1/6 z-50 absolute" src={addCircle} alt="add circle icon" />
+            </div>);
+    }
+    function SelectedPlayer() {
+        return (
+            <div
+                className="flex flex-col items-center justify-center w-1/6 mt-2 relative"
+            >
+                <img className="w-full" src={selectedShirt} alt="default T-shirt" />
+                <div className="flex flex-col w-full">
+                    <div>
+                        <div className="bg-[#37013B] py-1 px-4 text-white font-bold text-xs rounded-t-2xl flex items-center justify-center">Handerson</div>
+                        <div className="bg-white/50 font-bold text-sm rounded-b-2xl flex items-center justify-center">55</div>
+                    </div>
+                </div>
+            </div>);
+    }
+}
