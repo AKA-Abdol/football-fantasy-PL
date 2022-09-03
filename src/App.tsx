cac02762b3c311385a4b215f00b8457127b56a04
ali <mahmoodi.exp@gmail.com>
2022-09-03 17:07:45 +0430

    add player merged

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { RecoilRoot } from 'recoil';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import './App.css';
@@ -8,20 +9,22 @@ import Authentication from './pages/Authentication';
 
 function App() {
   return (
-    <div className="app">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="signup" element={<SignUp />} />
-          <Route path="signin" element={<SignIn />} />
-          <Route path="authentication" element={<Authentication/>} />
-          <Route path="prices" element={<Home />} />
-          <Route path="events" element={<Home />} />
-          <Route path="profile" element={<Home />} />
-          <Route path="sth" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <RecoilRoot>
+      <div className="app">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="signup" element={<SignUp />} />
+            <Route path="signin" element={<SignIn />} />
+            <Route path="authentication" element={<Authentication />} />
+            <Route path="prices" element={<Home />} />
+            <Route path="events" element={<Home />} />
+            <Route path="profile" element={<Home />} />
+            <Route path="sth" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </RecoilRoot>
   );
 }
 
