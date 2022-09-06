e27bb1c4eb6a1b43f38fdc83ac4187e48fc28d88
Sara <sara@carriot.ir>
2022-09-06 18:21:00 +0430

    set state for navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 import {Link} from 'react-router-dom'
+
+export type NavbarType = "MyTeam" | "Transfer" | "Event" | "Profile" | "Price"
 
 const Navbar = () => {
 
     const [showMenu, setShowMenu] = React.useState(false);
-
-
+    const [navbarState, setNavbarState] = useState<NavbarType>("MyTeam");
     const handleMenu = () => {
         setShowMenu(prevState => !prevState)
     }
@@ -16,13 +17,40 @@ const Navbar = () => {
         <div className="navbar w-full sm:max-w-[60%] bg-base-100 shadow-xl rounded-lg lg:-mt-6 z-50 font-semibold">
                 <div className="hidden text-xs w-full lg:flex lg:text-xl">
                     <ul className="menu menu-horizontal w-full flex flex-row justify-around">
-                        <li className="px-4"><Link to={"/prices"}>جوایز</Link></li>
-                        <li className="px-4"><Link to={"/profile"}>پروفایل</Link></li>
-                        <li className="px-4"><Link to={"/events"}>آخرین رویدادها</Link></li>
-                        <li className="px-4"><Link to={"/transfer"}>نقل و انتقالات</Link></li>
                         <li
-                            className="rounded-lg  bg-gradient-to-l from-detailListBoxColor1 to-detailListBoxColor2"
-
+                            onClick={() => {
+                                setNavbarState("Price")
+                            }}
+                            className = {`rounded-lg hover:rounded-lg` + 
+                            (navbarState === "Price" ? ` bg-gradient-to-l from-detailListBoxColor1 to-detailListBoxColor2 border-none ` : ` bg-base-100 `)}>
+                            <Link to={"/prices"}>جوایز</Link></li>
+                        <li 
+                            onClick={() => {
+                                setNavbarState("Profile")
+                            }}
+                            className = {`rounded-lg ` + 
+                            (navbarState === "Profile" ? ` bg-gradient-to-l from-detailListBoxColor1 to-detailListBoxColor2 border-none ` : ` bg-base-100 `)}>
+                            <Link to={"/profile"}>پروفایل</Link></li>
+                        <li 
+                            onClick={() => {
+                                setNavbarState("Event")
+                            }}
+                            className = {`rounded-lg ` + 
+                            (navbarState === "Event" ? ` bg-gradient-to-l from-detailListBoxColor1 to-detailListBoxColor2 border-none ` : ` bg-base-100 `)}>
+                            <Link to={"/events"}>آخرین رویدادها</Link></li>
+                        <li 
+                            onClick={() => {
+                                setNavbarState("Transfer")
+                            }}
+                            className = {`rounded-lg ` + 
+                            (navbarState === "Transfer" ? ` bg-gradient-to-l from-detailListBoxColor1 to-detailListBoxColor2 border-none ` : ` bg-base-100`)}>
+                            <Link to={"/transfer"}>نقل و انتقالات</Link></li>
+                        <li
+                            onClick={() => {
+                                setNavbarState("MyTeam")
+                            }}
+                            className = {`rounded-lg ` + 
+                            (navbarState === "MyTeam" ? ` bg-gradient-to-l from-detailListBoxColor1 to-detailListBoxColor2 border-none ` : ` bg-base-100`)}
                         ><Link to={"/"}>تیم من</Link></li>
                     </ul>
                 </div>
--- src/components/mainListComponents/Button.tsx
+++ src/components/mainListComponents/Button.tsx
@@ -10,7 +10,8 @@ interface MainListButtonProps{
 const MainListButton = ({name, inner, onclick, selected}: MainListButtonProps) => {
     return (
         <div className="flex flex-row-reverse mt-5">
-            <button name={name} onClick={onclick} className={`text-sm font-semibold py-1 border-2 rounded-lg border-borderItemColor w-10
+            <button name={name} onClick={onclick} 
+            className={`text-sm font-semibold py-1 border-2 rounded-lg border-borderItemColor w-10
                 text-black normal-case ml-10px` + 
                 (selected ? ` bg-gradient-to-l from-detailListBoxColor1 to-detailListBoxColor2 border-none ` : ` bg-white`)}>
                 {inner}
