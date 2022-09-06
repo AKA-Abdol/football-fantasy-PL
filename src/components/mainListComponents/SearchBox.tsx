f3a52577fdb2fa10be2949ea1ec5d80f3bbef5a0
thegrolam <thegrolam@gmail.com>
2022-09-06 06:52:20 +0000

    functionalities done

--- src/components/mainListComponents/SearchBox.tsx
+++ src/components/mainListComponents/SearchBox.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { atom, useSetRecoilState } from "recoil";
 import SearchIcon from "../../assets/search-normal.svg";
+import { currentPageAtom } from "./Pagination";
+import { DEFAULT_PAGE } from "./MainPlayerList";
 
 export const searchKeyAtom = atom({
     key: "SearchKey",
@@ -10,9 +12,11 @@ export const searchKeyAtom = atom({
 const SearchBox = () => {
 
     const setSearchKey = useSetRecoilState(searchKeyAtom);
+    const setCurrentPage = useSetRecoilState(currentPageAtom);
 
     const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
         setSearchKey(event.target.value);
+        setCurrentPage(DEFAULT_PAGE);
     }
 
     return (
