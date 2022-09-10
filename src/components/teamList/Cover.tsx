f52f0540f43e03dc6fd2970ddb7e28eef301725f
Sara <sara@carriot.ir>
2022-09-10 15:36:44 +0430

    team list

--- src/components/teamList/Cover.tsx
+++ src/components/teamList/Cover.tsx
@@ -11,14 +11,14 @@ const Cover = (name : Name) => {
     return (
         <div className="cover w-full z-50 h-full rounded-l-2xl bg-nameFontColor flex-col justify-center">
             <div className="shirt flex justify-center">
-                <img src={Shirt} alt="" />
+                <img src={Shirt} alt="shirt" />
 
             </div>
             <div className="league flex justify-center p-4 z-50">
-                <img src={League} alt="" />
+                <img src={League} alt="league" />
             </div>
             <div className="primary mt-10">
-                <img src={Primary} alt="" />
+                <img src={Primary} alt="Primary" />
             </div>
         </div>
     );
