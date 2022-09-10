0205e6dc2c654fdc77c1d469f2818ea0f54d40c7
Sara <sara@carriot.ir>
2022-09-10 16:24:46 +0430

    list

--- src/components/teamList/ListItem.tsx
+++ src/components/teamList/ListItem.tsx
@@ -14,10 +14,13 @@ const ListItem = (props : Detail) => {
                 {props.players.map(player => {
                     return (
                         <div className="item flex justify-between border-b-2 mr-7">
-                            <div className={`player-name font-semibold text-nameFontColor text-xs px-4 pt-1 pb-2`}>{player.type === "Field" ? player.name : "none"}</div>
+                            <div className = {`player-name font-semibold text-nameFontColor text-xs px-4 pt-1 pb-2` + 
+                                (player.type === "Default" ? ` font-normal ` : ` font-semibold `)}>
+                                    {player.type === "Field" ? player.name : "none"}
+                                </div>
                             <div className="flex justify-between w-1/4">
                                 <div className={`player-point font-semibold text-nameFontColor text-10 pt-1 pb-2`}>{player.type === "Field" ? player.score : "--"}</div>
-                                <div className={`player-price font-semibold text-nameFontColor text-10 pt-1 pb-2 ml-7`}>{player.type === "Field" ? player.price : "--"}</div>
+                                <div className={`player-price font-semibold text-nameFontColor text-10 pt-1 pb-2 ml-7`}>{player.type === "Field" ? player.price / 10 : "--"}</div>
                             </div>
                         </div>
                     )
