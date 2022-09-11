75b4414234e35e74f624a602b45ddcc6128c72d2
ali <mahmoodi.exp@gmail.com>
2022-09-11 11:22:47 +0430

    last main responsive

--- src/components/PageToggleTab.tsx
+++ src/components/PageToggleTab.tsx
@@ -8,7 +8,7 @@ interface ToggleTabProps {
 
 export default function PToggleTab({ currentTab, setSelTab }: ToggleTabProps) {
   return (
-    <div className="tabs tabs-boxed mx-auto h-full w-1/2 lg:w-full justify-center items-center bg-gray-100 theme-font -mt-[1px] -z-index-[50]">
+    <div className="tabs tabs-boxed mx-auto h-full w-[90%] lg:w-full justify-center items-center bg-gray-100 theme-font -mt-[1px] -z-index-[50]">
       <a
         className={`tab py-[3px] h-full px-1 w-1/2 rounded-l-lg text-[0.58rem] text-black hover:font-bold ${
           currentTab === 0 ? "bg-white font-bold" : null
