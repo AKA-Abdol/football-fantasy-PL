be3bbccb10a68e0bdd732f30f3c11b54aace4287
Sara <sara@carriot.ir>
2022-09-19 18:03:09 +0430

    responsive navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -19,7 +19,9 @@ const NavbarItem = (props: NavbarType, item: string) => {
                 setNavbarState(props)
             }}
             className = {`rounded-lg hover:bg-teal-200 hover:bg-none` + 
-            (navbarState === props ? ` bg-gradient-to-l from-detailListBoxColor1 to-detailListBoxColor2 border-none ` : ` bg-base-100 `)}>
+            (navbarState === props ? ` bg-gradient-to-l from-detailListBoxColor1
+             to-detailListBoxColor2 border-none ` 
+            : ` bg-base-100 `)}>
             <a><Link to={`/${endpoint}`}>{item}</Link></a>
         </li>
     )
@@ -34,13 +36,14 @@ const ResponsiveNavbarItem = (props: NavbarType, item: string) => {
             setNavbarState(props)
         }}
         className = {`px-14` + 
-        (navbarState === props ? ` rounded-lg px-20 bg-gradient-to-l from-detailListBoxColor1 to-detailListBoxColor2 ` : ` bg-base-100 `)}>
+        (navbarState === props ? ` rounded-lg w-64 justify-center text-gradient-to-l text-detailListBoxColor1 ` : ` bg-base-100 bg-inherit `)}>
         <Link to={`/${endpoint}`}>{item}</Link>
         </li>
     )
 }
 
 const Navbar = () => {
+    const navbarState = useRecoilState<NavbarType>(navbarStateAtom);
 
     const [showMenu, setShowMenu] = React.useState(false);
     const handleMenu = () => {
@@ -49,21 +52,21 @@ const Navbar = () => {
 
     
     return (
-        <div className="navbar w-full sm:max-w-[60%] bg-base-100 shadow-xl rounded-lg lg:-mt-6 z-50 font-semibold">
-                <div className="hidden text-xs w-full lg:flex lg:text-xl">
+        <div className="navbar w-full h-full sm:max-w-[60%]  bg-base-100 shadow-xl rounded-lg lg:-mt-6 z-50 font-semibold text-nameFontColor">
+                <div className="hidden text-xs w-full lg:flex lg:text-xl sm:hidden">
                     <ul className="menu  w-full flex flex-row justify-around rounded-box active:bg-none">
                         {NavbarItem("Award", "جوایز")}
                         {NavbarItem("Profile", "پروفایل")}
-                        {NavbarItem("Event", "آخرین رویدادها")}
+                        {NavbarItem("Event", "رویدادها")}
                         {NavbarItem("Transfer", "نقل و انتقالات")}
                         {NavbarItem("MyTeam", "تیم من")}
                     </ul>
                 </div>
-                <div className={showMenu ? "relative flex w-full lg:hidden" : "hidden"}>
-                    <ul className="menu menu-horizontal w-full flex flex-col-reverse text-xl justify-around items-center rounded-box">
+                <div className={showMenu ? "flex w-full lg:hidden" : "hidden"}>
+                    <ul className="menu menu-horizontal w-full h-full flex flex-col-reverse text-xl justify-around items-center rounded-box">
                         {ResponsiveNavbarItem("Award", "جوایز")}
                         {ResponsiveNavbarItem("Profile", "پروفایل")}
-                        {ResponsiveNavbarItem("Event", "آخرین رویدادها")}
+                        {ResponsiveNavbarItem("Event", "رویدادها")}
                         {ResponsiveNavbarItem("Transfer", "نقل و انتقالات")}
                         {ResponsiveNavbarItem("MyTeam", "تیم من")}
                         
@@ -72,7 +75,14 @@ const Navbar = () => {
                 <div
                     className={showMenu ? "hidden" : "w-full rounded-lg flex flex-row-reverse text-xl text-detailListBoxColor1 lg:hidden"}
 
-                ><a className="mx-auto text-2xl sm:text-3xl">تیم من</a></div>
+                ><a className="mx-auto text-2xl sm:text-3xl">
+                    {navbarState[0] === "Award" ? "جوایز":
+                    navbarState[0] === "MyTeam" ? "تیم من":
+                    navbarState[0] === "Event" ? "رویدادها":
+                    navbarState[0] === "Transfer" ? "نقل و انتقالات":
+                    navbarState[0] === "Profile"? "پروفایل" :
+                    "تیم من"}
+                </a></div>
                 <AiOutlineMenu size={40}
                     className={showMenu ? "hidden" : "flex lg:hidden cursor-pointer"}
                     onClick={handleMenu}
