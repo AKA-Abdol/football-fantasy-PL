cff14253993a9404620f706047300226fe550c2b
ali <mahmoodi.exp@gmail.com>
2022-09-20 10:03:27 +0430

    responsive events page

--- src/components/eventsComponents/EventItem.tsx
+++ src/components/eventsComponents/EventItem.tsx
@@ -4,11 +4,12 @@ import Avatar from "../../assets/WIN_20220906_19_33_10_Pro.jpg";
 import In from "../../images/Vector_in.svg";
 import Out from "../../images/Vector_out.svg";
 import Star from "../../images/Vector_star.svg";
+import PlayerSubstitution from "./PlayerSubstitution";
 const EventItem = () => {
   return (
     <div
       dir="rtl"
-      className="event-container p-1 bg-[#fbf] rounded-lg w-full flex"
+      className="max-w-xl event-container py-4 px-1 bg-[#fbfbfb] rounded-lg w-full flex"
     >
       <div className="avatar w-1/4 ml-auto flex flex-col justify-center items-center">
         <div className="avatar-photo w-1/2 rounded-full mb-1">
@@ -19,63 +20,34 @@ const EventItem = () => {
           <img src={Like} alt="like" />
         </div>
       </div>
-      <div className="detail w-3/4 bg-white rounded-lg  mr-auto pr-4 text-xs">
+      <div className="detail w-3/4 bg-white rounded-lg  mr-auto p-3 text-xs">
         <div className="score font-semibold flex flex-row items-center py-2">
           <div className="">امتیاز هفته</div>
-          <div className="score-bar flex flex-row items-center justify-around bg-gradient-to-l
+          <div className="score-bar flex flex-row align-middle items-center justify-around bg-gradient-to-l
            from-detailListBoxColor1 to-detailListBoxColor2 px-3 rounded mr-4">
-            <img src={Star} alt="vector" className="w-3 h-3 ml-1"/>
-            <p className="text-base">104</p>
+            <img src={Star} alt="vector" className="w-3 mb-[2px] ml-1" />
+            <p className="text-base mt-1">104</p>
           </div>
         </div>
         <div className="flex">
-        <div className="substitution-container w-1/3">
-          <p className="font-semibold mb-4">‌تعویض‌ها</p>
-          <div className="substitution flex justify-between">
-            <div className="col-1">
-              <div className="player-in">
-                <div className="flex mb-1">
-                  <img src={In} alt="vector" className="ml-2"/>
-                  <p>yasin</p>
-                </div>
-                <div className="flex mb-1">
-                  <img src={In} alt="vector" className="ml-2"/>
-                  <p>yasin</p>
-                </div>
-                <div className="flex mb-1">
-                  <img src={In} alt="vector" className="ml-2"/>
-                  <p>yasin</p>
-                </div>
-              </div>
-            </div>
-            <div className="col-2">
-              <div className="player_out">
-                <div className="flex mb-1">
-                    <img src={Out} alt="vector" className="ml-2"/>
-                    <p>yasin</p>
-                  </div>
-                  <div className="flex mb-1">
-                    <img src={Out} alt="vector" className="ml-2"/>
-                    <p>yasin</p>
-                  </div>
-                  <div className="flex mb-1">
-                    <img src={Out} alt="vector" className="ml-2"/>
-                    <p>yasin</p>
-                  </div>
-                </div>
+          <div className="substitution-container w-full">
+            <p className="font-semibold mb-4">‌تعویض‌ها</p>
+            <div className="substitution flex flex-col justify-between">
+              <PlayerSubstitution />
+              <PlayerSubstitution />
+              <PlayerSubstitution />
             </div>
           </div>
-        </div>
-        <div
-          className="week text-[#3d195b] bg-[#3d195b0f] h-1/3 p-1 mr-auto
+          <div
+            className="week text-[#3d195b] bg-[#3d195b0f] h-1/3 p-1 mr-auto
          rounded flex justify-center items-center -rotate-90 font-semibold text-2xs"
-        >
-          <p>#هفته_پنجم</p>
-        </div>
+          >
+            <p>#هفته_پنجم</p>
+          </div>
         </div>
       </div>
 
-      
+
     </div>
   );
 };
--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,19 +1,28 @@
-import React from 'react'
+import { atom, SetterOrUpdater, useRecoilState } from 'recoil'
 import EventItem from '../components/eventsComponents/EventItem'
+import EventTab, { EventTabToggleAtom } from '../components/eventsComponents/EventTab'
 import FollowButtons from '../components/eventsComponents/FollowButtons'
 import FollowItem from '../components/eventsComponents/FollowItem'
 import ListHeader from '../components/ListHeader'
 import MainListPagination from '../components/mainListComponents/Pagination'
 import SearchBox from '../components/mainListComponents/SearchBox'
 
-export default function events() {
+
+
+export default function Events() {
+
+    const [eventTabToggle, setEventTabToggle] = useRecoilState(EventTabToggleAtom);
+
+
+
     return (
-        <div className='flex flex-col p-10 w-full items-center '>
-            <div className='flex flex-col w-1/3 shadow-lg  space-y-2 relative'>
+        <div className='flex flex-col space-y-6 px-2 lg:px-40 py-10 w-full items-center '>
+            <div className='flex flex-col w-[90%] md:w-1/2 shadow-lg  space-y-2 relative'>
                 <SearchBox
                     whatToSearch='users'
                 />
                 <div className='hidden flex-col shadow-lg px-2 space-y-2 absolute w-full pb-2 z-50 bg-white top-11 rounded-b-lg'>
+
                     <FollowItem />
 
                     <FollowItem />
@@ -25,33 +34,38 @@ export default function events() {
 
 
             </div>
-            <div className="hidden lg:flex list mx-auto max-w-max flex-col  ml-auto rounded-2xl shadow-md mb-2 overflow-auto h-full">
-                <ListHeader
-                    text='دوستان شما'
-                />
-                <div className='w-full flex flex-col pt-6 pb-1 px-8 h-full '>
-
-                    <FollowButtons />
-                    <SearchBox
-                        whatToSearch='followList'
+            <h2 className='hidden lg:flex ml-auto font-black text-2xl text-[#3D195B]'>آخرید رویدادها</h2>
+            <EventTab />
+            <div className='flex lg:w-[95%] flex-col-reverse lg:flex-row lg:justify-between lg:space-x-6'>
+                <div className="flex list mx-auto max-w-max min-w-fit flex-col  ml-auto rounded-2xl shadow-md mb-2 overflow-auto h-full">
+                    <ListHeader
+                        text='دوستان شما'
                     />
+                    <div className='w-full flex flex-col pt-6 pb-1 px-8 h-full '>
 
-                    <div className='w-full py-2'>
-                        <FollowItem />
+                        <FollowButtons />
+                        <SearchBox
+                            whatToSearch='followList'
+                        />
+
+                        <div className='w-full py-2'>
+                            <FollowItem />
+                        </div>
+
+
+
+                        <MainListPagination />
                     </div>
 
-
-
-                    <MainListPagination />
+                </div >
+                <div className='w-full flex flex-col items-end space-y-4'>
+                    <EventItem />
+                    <EventItem />
+                    <EventItem />
+                    <EventItem />
                 </div>
-
-            </div >
-            <div className='w-full'>
-                <EventItem/>
-                <EventItem/>
-                <EventItem/>
-                <EventItem/>
             </div>
+
         </div>
 
     )
