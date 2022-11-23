4db23281e7cd755bbc74504cac088725c249df93
Amirhossein <amirabdolii@gmail.com>
2022-11-24 01:49:10 +0330

    sara special header

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,16 +1,17 @@
 import React from "react";
-import headerBg from "./../images/headerBg.svg"
-import fplLogo from "./../images/fplLogo.svg"
-import players from "./../images/players.png"
-import bgLayer from "./../images/bgLayer.png"
+import headerBg from "./../images/headerBg.svg";
+import fplLogo from "./../images/fplLogo.svg";
+import players from "./../images/players.png";
+import bgLayer from "./../images/bgLayer.png";
 import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
+import Typist from "react-typist";
 
 export default function PageHeader() {
-    return (
+  return (
     <div className="flex flex-col w-full theme-font items-center relative">
-        <div className="flex flex-col justify-center items-center w-full">
-            <div
+      <div className="flex flex-col justify-center items-center w-full">
+        {/* <div
                 className="relative h-full flex flex-row w-full bg-center bg-no-repeat bg-cover"
                 style={{ backgroundImage: `url(${headerBg})` }}
             >
@@ -31,11 +32,31 @@ export default function PageHeader() {
                     />
                 </div>
 
-            </div>
-            <Navbar />
-        </div>
-        <Outlet/>
-    </div>
+            </div> */}
+        <div className="flex flex-col justify-center items-center w-full text-xl font-extrabold bg-[#03FBB8] text-black" dir="rtl">
+          <Typist className="my-8 text-[#3D195B]">
 
-    )
+            <span>الان حوالی ۱:۳۰ صبحه و خستم واقعا</span>
+            <Typist.Backspace count={35} delay={3000} />
+            <span>احتمالا ۸ ۹ ساعت دیگه داری اینو میخونی</span>
+            <Typist.Backspace count={42} delay={3000} />
+            <span>منم دیگه فکرم کار نمیکنه و جملات آخره</span>
+            <Typist.Backspace count={42} delay={3000} />
+            <span>از اون روز خاص هم چند روزی گذشته</span>
+            <Typist.Backspace count={42} delay={3000} />
+            <span>اما این باعث نمیشه که ما یادمون بره</span>
+            <Typist.Backspace count={42} delay={3000} />
+            <span>دختر sensitive گروه عجیب High5</span>
+            <Typist.Backspace count={42} delay={3000} />
+            <span>تولدت خیلی خیلی مبارکه</span>
+            <Typist.Backspace count={32} delay={2000} />
+            <span>*--* تولدت مبارک سارا *--*</span>
+
+          </Typist>
+        </div>
+        <Navbar />
+      </div>
+      <Outlet />
+    </div>
+  );
 }
